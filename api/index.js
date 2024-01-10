07551aa7cabc1811c437a462af14263584db72c0
Srijan <srijan.kumar2021@gmail.com>
2024-01-10 12:41:01 +0530

    front-end as well as beck-end

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('COnnected to db');
+})
+.catch((err) =>{
+    console.log(err);
+});
+
+const app = express();
+
+app.listen(3000, () =>{
+    console.log("Server is running on 3000")
+}
+
+);
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next )=> {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
