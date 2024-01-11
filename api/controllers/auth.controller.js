a64b844eaaab07a7cd6838a05481a143993c3823
Srijan <srijan.kumar2021@gmail.com>
2024-01-11 20:53:32 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,34 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs'; 
-import { errorHandler } from '../utils/error.js';
-
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"; ////used to create cookie
 
 export const signup = async (req, res, next) => {
-    const {username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword});
-    try{
-        await newUser.save();
-        res.status(201).json("User created successfully");
-    }catch (error){
-        next(error);
-    }
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+////////////////////////////////////////////////////////////////
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const ValidPass = bcryptjs.compareSync(password, validUser.password);
+    if (!ValidPass) return next(errorHandler(401, "Wrong Credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; //this line will be helpful in hiding the password like after this line out password will not be returned
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
