8581e65c14d2d5084a217baa80a0e7c6203a873e
Srijan <srijan.kumar2021@gmail.com>
2024-01-15 14:42:06 +0530

    update user api

--- api/index.js
+++ api/index.js
@@ -1,35 +1,38 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('COnnected to db');
-})
-.catch((err) =>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("COnnected to db");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("Server is running on 3000")
-}
-
-);
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
+});
 
 app.use(express.json());
-app.use("/api/user", userRouter);
-app.use('/api/auth', authRouter);
 
-app.use((err, req, res, next )=> {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+app.use(cookieParser());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
 });
