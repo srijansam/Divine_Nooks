1566e6a55ffaf528c7a72f5dd24bb243cdde7140
Srijan <srijan.kumar2021@gmail.com>
2024-02-14 00:48:09 +0530

    listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
