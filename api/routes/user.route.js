3cd1cc40057416c00ec04d9de28657b1210f9026
Srijan <srijan.kumar2021@gmail.com>
2024-02-17 10:16:28 +0530

    complete show user listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   deleteUser,
   test,
-  updateUser
+  updateUser,
+  getUserListings
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
