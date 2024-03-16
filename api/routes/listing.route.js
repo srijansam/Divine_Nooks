1f5597cb25e988771fc4d802430512f381538656
Srijan <srijan.kumar2021@gmail.com>
2024-03-17 00:18:07 +0530

    upto adding image slider

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createListing,
-  deleteListing
+  deleteListing,
+  updateListing,
+  getListing
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 
 export default router;
